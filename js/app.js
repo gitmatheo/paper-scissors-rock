08adf434c65d0e06acc2bb38586e6f3bb40abce4
gitmatheo <matteuszd@gmail.com>
2018-12-18 02:21:39 +0100

    Start screen added, animations modified, counters fixed

--- js/app.js
+++ js/app.js
@@ -10,25 +10,74 @@ $(document).ready(function() {
 
   const arr = ["rock", "scissors", "paper"];
 
-  // Computer selects a random number between 1 and 3.
+  function showGameScreen() {
+    $(".play")
+      .removeClass("play")
+      .addClass("flash-fast");
+    const first = setTimeout(() => {
+      $(".play").addClass("none");
+      $("#main").fadeIn("slow");
+      $("#main").removeClass("none");
+      clearInterval(first);
+    }, 1000);
+  }
+
   function computerSelection() {
     const randomNumber = Math.floor(Math.random() * 3 + 1);
     const computerChoice = arr[randomNumber - 1];
-    console.log("console log " + computerChoice);
     changeCompImage(computerChoice);
     return computerChoice;
   }
 
+  function disableScreenFor(milisec) {
+    $(".footer > img").css("pointer-events", "none");
+    setTimeout(function() {
+      $(".footer > img").css("pointer-events", "auto");
+    }, milisec);
+  }
+
+  function countdown(sec) {
+    let i = sec;
+
+    const count = setInterval(() => {
+      i--;
+      if (i >= 1) {
+        const result = $(".result").text(i);
+        return result;
+      } else {
+        const again = $(".result").text("Play Again");
+        clearInterval(count);
+        return again;
+      }
+    }, 1000);
+  }
+
+  function playAgain() {
+    $(".result")
+      .text(countdown(4))
+      .addClass("play");
+  }
+
+  function resetCounters() {
+    playerPoints = 0;
+    computerPoints = 0;
+    $(".p-points").text(playerPoints);
+    $(".c-points").text(computerPoints);
+    roundCounter = 0;
+    $(".r-counter").text(roundCounter);
+  }
+
   function playRound(playerSelection, computerSelection) {
+    $(".result").removeClass("play");
     roundCounter++;
     $(".r-counter").text(roundCounter);
+
     if (
       (playerSelection == "paper" && computerSelection == "rock") ||
       (playerSelection == "scissors" && computerSelection == "paper") ||
       (playerSelection == "rock" && computerSelection == "scissors")
     ) {
       $(".result").text("You win");
-      console.log("You win");
       playerPoints++;
       $(".p-points").text(playerPoints);
     } else if (
@@ -37,36 +86,19 @@ $(document).ready(function() {
       (playerSelection == "rock" && computerSelection == "paper")
     ) {
       $(".result").text("You lose");
-      console.log("You lose");
       computerPoints++;
       $(".c-points").text(computerPoints);
     } else if (playerSelection == computerSelection) {
       $(".result").text("Draw");
-      console.log("Draw");
-    } else {
-      console.log("ERROR! - choose 'rock, 'paper' or 'scissors'");
     }
 
     if (computerPoints == 5 || playerPoints == 5) {
       //RESETING COUNTERS
       $(".result").text("GAME OVER");
-      playerPoints = 0;
-      computerPoints = 0;
-      $(".p-points").text(playerPoints);
-      $(".c-points").text(computerPoints);
-      roundCounter = 0;
-      $(".r-counter").text(roundCounter);
-    } else {
-      console.log(
-        "Your selection: " +
-          playerSelection +
-          " Computer selection: " +
-          computerSelection +
-          " Your points: " +
-          playerPoints +
-          " Computer points: " +
-          computerPoints
-      );
+      resetCounters();
+      disableScreenFor(5000);
+      playAgain();
+      // showResultScreen();
     }
   }
 
@@ -92,7 +124,6 @@ $(document).ready(function() {
     let animationName = "animated wobble";
     let animationEnd =
       "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-
     $(".computer > img")
       .addClass(animationName)
       .one(animationEnd, function() {
@@ -101,13 +132,9 @@ $(document).ready(function() {
   }
 
   function game() {
-    console.log(playerPoints);
-    console.log(computerPoints);
-
-    let animationName = "animated pulse";
+    let animationName = "animated bounceIn";
     let animationEnd =
       "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-
     $(this)
       .addClass(animationName)
       .one(animationEnd, function() {
@@ -117,8 +144,8 @@ $(document).ready(function() {
     playRound(alt, computerSelection());
     changePlayerImage(alt);
   }
-
+  $(".play")
+    .text("Play")
+    .click(showGameScreen);
   $(".footer > img").click(game);
-
-  $();
 });
